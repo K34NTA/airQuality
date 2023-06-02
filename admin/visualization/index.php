59c7784d4fed2e9ee4eac26c9530660eadd96457
K34NTA <80090483+K34NTA@users.noreply.github.com>
2023-06-02 01:33:47 -0700

    updating the template

--- admin/visualization/index.php
+++ admin/visualization/index.php
@@ -4,6 +4,8 @@
 <head>
     <?php include ('../includes/header.php'); ?>
     <title>Data Visualization</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
 </head>
 
 <body id="page-top">
@@ -29,11 +31,115 @@
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                    <h1 class="h3 mb-4 text-gray-800"><strong>Data Visualization</strong></h1>
+                    <div class="row">
 
+                        <div class="col-lg-6">
+
+                            <!-- Carbon Monoxide -->
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Carbon Monoxide</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="chart-area">
+                                        <canvas id="myAreaChart"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <!-- Nitrogen Dioxide -->
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Nitrogen Dioxide</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="chart-bar">
+                                        <canvas id="myBarChart"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <!-- Ground-level Ozone -->
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Ground-level Ozone</h6>
+                                </div>
+                                <div class="card-body">
+                                <canvas id="myChart"></canvas>
+                                <script>
+                                    var ctx = document.getElementById('myChart').getContext('2d');
+                                    var myChart = new Chart(ctx, {
+                                        type: 'line',
+                                        data: {
+                                        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+                                        datasets: [{
+                                            label: 'Sales',
+                                            data: [1200, 1800, 800, 1500, 2000, 1300],
+                                            fill: false,
+                                            borderColor: 'rgba(54, 162, 235, 1)',
+                                            tension: 0.1
+                                        }]
+                                        },
+                                        options: {
+                                        scales: {
+                                            y: {
+                                            beginAtZero: true
+                                            }
+                                        }
+                                        }
+                                    });
+                                </script>
+                                </div>
+                            </div>
+
+                            <!-- Particulate Matter-->
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Particulate Matter</h6>
+                                </div>
+                                <div class="card-body">
+                                    <canvas id="myChart2"></canvas>
+                                    <script>
+                                        var ctx = document.getElementById('myChart2').getContext('2d');
+                                        var myChart2 = new Chart(ctx, {
+                                            type: 'line',
+                                            data: {
+                                            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+                                            datasets: [{
+                                                label: 'Data 1',
+                                                data: [10, 20, 15, 25, 18, 30],
+                                                fill: false,
+                                                borderColor: 'rgba(54, 162, 235, 1)',
+                                                tension: 0.1
+                                            },
+                                            {
+                                                label: 'Data 2',
+                                                data: [5, 15, 10, 20, 12, 25],
+                                                fill: false,
+                                                borderColor: 'rgba(255, 99, 132, 1)',
+                                                tension: 0.1
+                                            }]
+                                            },
+                                            options: {
+                                            scales: {
+                                                y: {
+                                                beginAtZero: true
+                                                }
+                                            }
+                                            }
+                                        });
+                                    </script>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <!-- /.container-fluid -->
-
             </div>
             <!-- End of Main Content -->
 
