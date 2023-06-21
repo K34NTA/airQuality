701b9d4e61c954b0965472833cb746b42e964803
K34NTA <kntngltyc@gmail.com>
2023-06-21 13:09:05 +0800

    adding information in the about page

--- admin/about/index.php
+++ admin/about/index.php
@@ -26,10 +26,74 @@
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                <div class="container-fluid">
+                <div class="container-fluid" style="color:#00703C; padding-left: 15px;">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">About</h1>
+                    <div>
+                        <h2> <strong>Air Pollutant Effects</strong> </h2>
+                        <div style="padding-left:20px">
+                            <h2>Carbon Monoxide (CO)</h2>
+                            <ul>
+                            <li>Inhalation of carbon monoxide can lead to headaches, dizziness, and confusion.</li>
+                            <li>High levels of exposure can cause impaired vision, reduced manual dexterity, and loss of consciousness.</li>
+                            <li>Prolonged or severe exposure to carbon monoxide can result in neurological damage, cardiovascular problems, and even death.</li>
+                            </ul>
+
+                            <h2>Nitrogen Dioxide (NO2)</h2>
+                            <ul>
+                            <li>Short-term exposure to nitrogen dioxide can cause respiratory irritation, coughing, wheezing, and shortness of breath.</li>
+                            <li>Prolonged exposure to high levels of nitrogen dioxide can contribute to the development or worsening of respiratory conditions, such as asthma and bronchitis.</li>
+                            <li>In individuals with pre-existing cardiovascular diseases, nitrogen dioxide exposure may increase the risk of heart attacks.</li>
+                            </ul>
+
+                            <h2>Ground-Level Ozone (O3)</h2>
+                            <ul>
+                            <li>Inhalation of ground-level ozone can cause throat irritation, coughing, chest discomfort, and shortness of breath.</li>
+                            <li>Prolonged exposure to high levels of ozone can lead to reduced lung function, increased respiratory infections, and the aggravation of asthma and other respiratory conditions.</li>
+                            <li>Ozone exposure is particularly harmful to children, the elderly, and individuals with respiratory diseases.</li>
+                            </ul>
+
+                            <h2>Particulate Matter (PM)</h2>
+                            <ul>
+                            <li>Inhalation of particulate matter can irritate the respiratory system, causing coughing, wheezing, and shortness of breath.</li>
+                            <li>Fine particulate matter (PM2.5) can penetrate deep into the lungs and enter the bloodstream, leading to cardiovascular problems, including heart attacks and strokes.</li>
+                            <li>Long-term exposure to high levels of particulate matter has been associated with the development of respiratory and cardiovascular diseases, and it can also worsen existing conditions.</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <br><br>
+                    <h2 class="mb-4"><strong>Student's Information</strong></h2>
+                    <div class="row" style="padding-left:20px">
+                        <div class="col-sm-6">
+                            <img src="../../assets/img/tayco.png" alt="Person's Photo" style="height: 200px; width: 200px;">
+                            <h1>Kent Angelo Tayco</h1>
+                            <h3>Student</h3>
+                            <p>A 4th year Computer Engineering student from La Salle University -  Ozamiz</p>
+                            <ul>
+                            <li><strong>Address:</strong> Dumingag, Zamboanga del Sur</li>
+                            <li><strong>Personal Email:</strong> kntngltyc@gmail.com</li>
+                            <li><strong>Schoool Email:</strong> kent.tayco@lsu.edu.ph</li>
+                            <li><strong>Phone:</strong> +63 929 296 3167</li>
+                            <li><strong>Website:</strong> <a href="https://www.linkedin.com/in/kent-angelo-tayco-a52262270/">LinkedIn</a></li>
+                            <!-- Add more details as needed -->
+                            </ul>
+                        </div>
+                        <div class="col-sm-6">
+                            <img src="../../assets/img/roque.png" alt="Person's Photo" style="height: 200px; width: 200px;">
+                            <h1>Paul Roque</h1>
+                            <h3>Student</h3>
+                            <p>A 4th year Computer Engineering student from La Salle University -  Ozamiz</p>
+                            <ul>
+                            <li><strong>Address:</strong> Aloran, Misamis Occidental</li>
+                            <li><strong>Personal Email:</strong> paulfrq1986@gmail.com</li>
+                            <li><strong>School Email:</strong> paul.roque@lsu.edu.ph</li>
+                            <li><strong>Phone:</strong> +63 995 657 4209</li>
+                            <li><strong>Website:</strong> <a href="https://www.facebook.com/wmnnwlhtscuclh">Facebook</a></li>
+                            <!-- Add more details as needed -->
+                            </ul>
+                        </div>
+                    </div>
 
                 </div>
                 <!-- /.container-fluid -->
