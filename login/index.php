3969d5400e9d8095f9570d5b66b516831454507a
K34NTA <kntngltyc@gmail.com>
2023-11-19 22:18:32 +0800

    updating the design

--- login/index.php
+++ login/index.php
@@ -19,81 +19,83 @@
     <!-- Custom styles for this template-->
     <link href="<?php echo base_url ?>assets/css/sb-admin-2.min.css" rel="stylesheet">
     <style>
-        body, html {
-            margin: 0;
-            padding: 0;
+   body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: url('../assets/img/aqi.png');
+    background-size: cover; /* Use cover for responsive background images */
+}
+
+@media only screen and (max-width: 600px) {
+    body {
+        background-size: contain; /* Adjust as needed */
+    }
+}
+
+
+        @keyframes borderAnimation {
+            0% {
+                border-color: rgb(255, 0, 0);
+            }
+            25% {
+                border-color: rgb(0, 255, 0);
+            }
+            50% {
+                border-color: rgb(0, 0, 255);
+            }
+            75% {
+                border-color: rgb(255, 255, 0);
+            }
+            100% {
+                border-color: rgb(255, 0, 255);
+            }
         }
 
-        .background-image {
-            position: fixed;
-            top: 0;
-            left: 0;
+        .login-form {
+            background-color: rgba(18, 250, 219, 0.3);
+            padding: 20px;
+            border-radius: 30px;
+            max-width: 400px;
             width: 100%;
-            height: 100%;
-            background-image: url('../assets/img/aqi.png');
-            background-size: cover;
-            background-position: center;
-            z-index: -1; 
+            border: 6px solid transparent;
+            animation: borderAnimation 5s infinite; 
         }
+
+      
 
         .content {
             padding: 20px;
         }
+        
     </style>
 </head>
 
 <body>
-    <div class="background-image"></div>
-    <div class="row">
-        <!-- Outer Row -->
-        <div class="justify-content-center">
-            <div class="background-image">
-                <div class="row">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="col-md-4" >
-                            <div class="text-center">
-                                <h1 style="color:#ffffff;">
-                                    <strong style="font-family: none;">Welcome Back!</strong>
-                                </h1>
-                            </div>
-                            <form class="user" action="logincode.php" method="POST">
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" name="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-user" name="password" id="exampleInputPassword" placeholder="Password">
-                                </div>
-                                <!-- <div class="form-group">
-                                    <div class="custom-control custom-checkbox small">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                                        <label class="custom-control-label" for="customCheck">Remember Me</label>
-                                    </div>
-                                </div> -->
-                                <button type="submit" name="login_btn" class="btn btn-success btn-user btn-block">Login</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <div class="login-form">
+        <div class="text-center">
+         <h1 style="color:hwb(0 100% 0%);">
+                <strong style="font-family: none;">Welcome Back!</strong>
+            </h1>
         </div>
+        <form class="user" action="logincode.php" method="POST">
+            <div class="form-group">
+                <input type="email" class="form-control form-control-user" name="email" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control form-control-user" name="password" id="exampleInputPassword" placeholder="Password">
+            </div>
+            <button type="submit" name="login_btn" class="btn btn-success btn-user btn-block">Login</button>
+        </form>
     </div>
-    <?php include ('message.php'); ?>
-    <script>
-      var base_url = "<?php echo base_url ?>"; // global location for javascript
-    </script>
-    <!-- SweetAlert -->
-    <script src="<?php echo base_url ?>assets/js/sweetalert.js"></script>
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?php echo base_url ?>assets/vendor/jquery/jquery.min.js"></script>
-    <script src="<?php echo base_url ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="<?php echo base_url ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="<?php echo base_url ?>assets/js/sb-admin-2.min.js"></script>
-
+    <!-- Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
