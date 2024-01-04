1a4d4b72f3e3592332c519e3b363160d8c20064b
K34NTA <kntngltyc@gmail.com>
2024-01-04 15:54:05 +0800

    fixing the no login

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="refresh" content="0; URL='./login'" /> 
+        <meta http-equiv="refresh" content="0; URL='./admin'" /> 
     </head>
 </html>
