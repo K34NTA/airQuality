f817141e1e161dd6d9a3623619dc9da437a3f53e
K34NTA <kntngltyc@gmail.com>
2024-01-09 23:04:43 +0800

    adding the tutorial page

--- admin/tutorial/index.php
+++ admin/tutorial/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php include ('../includes/header.php'); ?>
-    <title>SB Admin 2 - Blank</title>
+    <title>Website User Guide</title>
 </head>
 
 <body id="page-top">
@@ -38,32 +38,86 @@
                                 <div class="container-fluid" style="padding-top: 20px;">
                                     <p class="font-weight-bold">1. Dashboard page</p>
                                     <p style="padding-left:20px;"> 1.1 Charts </p>
-                                    <p style="padding-left:40px;"> 1.1.1 Line Chart</p>
-                                    <center>
-                                    <p style="padding-left:60px;"> The first line chart displays three gas data</p>
-                                    <p style="padding-left:60px;"> Click on the legends to focus on individual gases</p>
-                                    <p style="padding-left:60px;"> To exclude a gas from the chart, click its legend again.</p>                                        
+                                    <p style="padding-left:40px;"> 1.1.1 Line chart</p>
+                                    <center> 
+                                        <img src="../../assets/img/t1.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> The first line chart displays three gas data</p>
+                                        <img src="../../assets/img/t2.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Click on the legends to focus on individual gases</p>
+                                        <img src="../../assets/img/t3.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> To reinclude a gas data from the chart, click its legend again.</p>                                        
                                     </center>
                                     <p style="padding-left:40px;"> 1.1.2 Dust data line chart</p>
                                     <center>
-                                    <p style="padding-left:60px;"> This separate line chart displays dust data. No action needed to view it clearly.</p>                                    
+                                        <img src="../../assets/img/t4.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> This chart displays dust data. No action needed to view it clearly.</p>                                    
                                     </center>
-                                    <p style="padding-left:40px;"> 1.1.3 Bar Chart</p>
+                                    <p style="padding-left:40px;"> 1.1.3 Bar chart</p>
                                     <center>
-                                    <p style="padding-left:60px;"> Displays the latest sensor data. All data points are presented in the bar chart.</p>                                    
+                                        <img src="../../assets/img/t5.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Displays the latest data. All data points are presented in the bar chart.</p>                                    
                                     </center>
-                                    <p style="padding-left:20px;"> 1.2 User Guide</p>
+                                    <p style="padding-left:20px;"> 1.2 User guide</p>
                                     <center>
-                                    <p style="padding-left:60px;"> Find tips on how to interpret and navigate the charts effectively.</p>                                    
+                                        <img src="../../assets/img/t6.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:40px;"> Find tips on how to interpret and navigate the charts effectively.</p>                                    
                                     </center>
                                     <p class="font-weight-bold">2. Activity Log page</p>
                                     <p style="padding-left:20px;"> 2.1 Tables </p>
-                                    <p style="padding-left:40px;"> 2.1.1 Gas Quality Table</p>
-                                    <p style="padding-left:40px;"> 2.1.2 Overall Data Table</p>
-                                    <p style="padding-left:40px;"> 2.1.3 Date Range Table</p>
-                                    <p style="padding-left:20px;"> 2.2.1 Download data</p>
+                                    <p style="padding-left:40px;"> 2.1.1 Gas Quality table</p>
+                                    <center>
+                                        <img src="../../assets/img/t7.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Displays data categorization as good or unhealthy for gases.</p>                                        
+                                    </center>
+                                    <p style="padding-left:40px;"> 2.1.2 Overall Data table</p>
+                                    <center>
+                                        <img src="../../assets/img/t8.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Shows all collected data, sortable by date and data value.</p>
+                                        <img src="../../assets/img/t9.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Click the top row to sort by ascending or descending order.</p>
+                                        <img src="../../assets/img/t10.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Use the search part to check data.</p>                                        
+                                    </center>
+                                    <p style="padding-left:40px;"> 2.1.3 Date Average table</p>
+                                    <center>
+                                        <img src="../../assets/img/t11.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Provides total data collected in a day with a date format search bar.</p>
+                                        <img src="../../assets/img/t12.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Choose the date range for the desired data.</p>
+                                        <img src="../../assets/img/t13.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Click “Search” in order to display the data.</p>
+                                        <img src="../../assets/img/t14.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> All data were in the date range will be displayed.</p>
+                                        <img src="../../assets/img/t15.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Check the bar chart for the average data of the date range searched.</p>
+                                        <img src="../../assets/img/t16.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Click the legend of the average data to exclude.</p>
+                                        <img src="../../assets/img/t17.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:60px;"> Click the legend again to show the average data of the gas.</p>                                        
+                                    </center>
+                                    <p style="padding-left:20px;"> 2.2 Download button</p>
+                                    <center>
+                                        <img src="../../assets/img/t18.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:40px;"> Allows users to download data within a specified date range.</p>
+                                        <img src="../../assets/img/t19.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:40px;"> The downloaded data will be in csv file format.</p>                                        
+                                    </center>
                                     <p class="font-weight-bold">3. Air Quality Index page</p>
+                                    <center>
+                                        <img src="../../assets/img/t20.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:20px;"> This shows specific levels of the data in identifying the affects to a person's health.</p>                                        
+                                    </center>
                                     <p class="font-weight-bold">4. About page</p>
+                                    <p style="padding-left:20px;"> 4.1 Air Pollutant effects</p>
+                                    <center>
+                                        <img src="../../assets/img/t21.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:40px;"> This shows the specific levels of the gas on how toxic it was.</p>                                        
+                                    </center>
+                                    <p style="padding-left:20px;"> 4.2 Student's Information</p>
+                                    <center>
+                                        <img src="../../assets/img/t22.png" alt="Person's Photo" style="height: 100%; width: 70%;">
+                                        <p style="padding-left:40px;"> This shows the students that are part of making the project and their personal information.</p>                                       
+                                    </center>
                                 </div>
                             </div>
                         </div>
